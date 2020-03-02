e0907d183cb12e493ef73e9b36088e6bb5c28f01
Luis Pestana <lpestana@protonmail.com>
2020-03-02 22:46:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,22 @@ require "yaml"
 def load_library(file_path)
   # code goes here
   library = YAML.load_file(file_path)
-  # pp library
+  # library.each_value { |(english, japanese)| puts "#{english} and #{japanese}" }
+  hash = {
+    'get_meaning' => create_hash(library, 'japanese'),
+    'get_emoticon' => create_hash(library, 'english')
+  }
+  pp hash
+end
+
+def create_hash(library, language)
+  hash = {}
+  if language == 'english'
+    library.each_value { |(english, japanese)| hash[english] = "#{japanese}" }
+  else
+    library.each_value { |(english, japanese)| hash[japanese] = "#{english}" }
+  end
+  hash
 end
 
 def get_japanese_emoticon
