e06322ae89d848fa4df65f5b3ec3d783dbd99241
Luis Pestana <lpestana@protonmail.com>
2020-03-02 23:10:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if hash['get_emoticon'][emoticon]
     return hash['get_emoticon'][emoticon]
   else
-    return 'Very sorry, that emoticon could not be found'
+    return 'Sorry, that emoticon was not found'
   end
 end
 
@@ -38,6 +38,6 @@ def get_english_meaning(file_path, emoticon)
   if hash['get_meaning'][emoticon]
     return hash['get_meaning'][emoticon]
   else
-    return 'Very sorry, that emoticon could not be found'
+    return 'Sorry, that emoticon was not found'
   end
 end
